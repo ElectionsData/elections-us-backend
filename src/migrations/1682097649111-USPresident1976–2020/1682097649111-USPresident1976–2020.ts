cd4fc5459e4fcfa2baf28c285e341251ea08dc24
bendgomin333 <bendgomin333@gmail.com>
2023-04-22 11:53:09 +0300

    refactoring

--- src/migrations/1682097649111-USPresident1976–2020/1682097649111-USPresident1976–2020.ts
+++ src/migrations/1682097649111-USPresident1976–2020/1682097649111-USPresident1976–2020.ts
@@ -2,19 +2,15 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 import * as csvParser from 'csv-parser';
 import * as fs from 'fs';
 
-export class AddHarvardData1682097649111 implements MigrationInterface {
+export class USPresident1976_2020_1682097649111 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    const stream = fs.createReadStream(__dirname + '/harvard.csv').pipe(
-      csvParser({
-        mapHeaders: ({ header }) => header.toLowerCase(),
-      }),
-    );
-
-    // datasets with unique values
-    const states = new Set();
-    const parties = new Set();
-    const candidates = new Set();
-    const elections = new Set();
+    const stream = fs
+      .createReadStream(__dirname + '/1682097649111-USPresident1976-2020.csv')
+      .pipe(
+        csvParser({
+          mapHeaders: ({ header }) => header.toLowerCase(),
+        }),
+      );
 
     // temporary variable, initial value
     const stateElectionResults = {
@@ -28,48 +24,32 @@ export class AddHarvardData1682097649111 implements MigrationInterface {
       const candidate = row.candidate.replaceAll("'", "''");
 
       // Add unique state to db
-      if (!states.has(row.state)) {
-        await queryRunner.query(
-          `INSERT INTO state (name)
-          SELECT '${row.state}'
-          EXCEPT
-          SELECT name FROM state`,
-        );
-        states.add(row.state);
-      }
+      await queryRunner.query(
+        `INSERT INTO state (id, name)
+          SELECT ${row.state_fips}, '${row.state}'
+          EXCEPT SELECT id, name FROM state`,
+      );
 
       // Add unique party to db
-      if (!parties.has(row.party_simplified)) {
-        await queryRunner.query(
-          `INSERT INTO party (name)
+      await queryRunner.query(
+        `INSERT INTO party (name)
           SELECT '${party}'
-          EXCEPT
-          SELECT name FROM party`,
-        );
-        parties.add(row.party_simplified);
-      }
+          EXCEPT SELECT name FROM party`,
+      );
 
       // Add unique candidate to db
-      if (!candidates.has(row.candidate + row.party_simplified)) {
-        await queryRunner.query(
-          `INSERT INTO candidate (name, party_id)
+      await queryRunner.query(
+        `INSERT INTO candidate (name, party_id)
           SELECT '${candidate}', (SELECT id FROM party WHERE party.name = '${party}')
-          EXCEPT
-          SELECT name, party_id FROM candidate`,
-        );
-        candidates.add(row.candidate + row.party_simplified);
-      }
+          EXCEPT SELECT name, party_id FROM candidate`,
+      );
 
       // Add unique election to db
-      if (!elections.has(row.office + row.year)) {
-        await queryRunner.query(
-          `INSERT INTO election (name, type, year)
+      await queryRunner.query(
+        `INSERT INTO election (name, type, year)
             SELECT 'U.S. President ${row.year}', 'Federal elections', ${row.year}
-            EXCEPT
-            SELECT name, type, year FROM election`,
-        );
-        elections.add(row.office + row.year);
-      }
+            EXCEPT SELECT name, type, year FROM election`,
+      );
 
       // Add state election result to db
       await (async function addStateElectionResult() {
@@ -84,15 +64,13 @@ export class AddHarvardData1682097649111 implements MigrationInterface {
           )[0];
 
           const winnerName = winnerRow.candidate.replaceAll("'", "''");
-
           const qElectionId = `(SELECT id FROM election WHERE election.year = ${stateElectionResults.year})`;
-          const qStateId = `(SELECT id FROM state WHERE state.name = '${stateElectionResults.state}')`;
           const qCandidatePartyId = `(SELECT id FROM party WHERE party.name = '${winnerRow.party_simplified}')`;
           const qWinnerId = `(SELECT id FROM candidate WHERE candidate.name = '${winnerName}' AND candidate.party_id = ${qCandidatePartyId})`;
 
           await queryRunner.query(
             `INSERT INTO state_election_result (election_id, state_id, winner_id, total_votes, candidate_votes)
-            SELECT ${qElectionId}, ${qStateId}, ${qWinnerId}, ${winnerRow.totalvotes}, '{"":""}'::jsonb
+            SELECT ${qElectionId}, ${winnerRow.state_fips}, ${qWinnerId}, ${winnerRow.totalvotes}, '{"":""}'::jsonb
             EXCEPT
             SELECT election_id, state_id, winner_id, total_votes, candidate_votes FROM state_election_result`,
           );
